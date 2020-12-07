d66113102e0f79ed4b3b2cd04d87a2365558c41c
Johnsocar <mathias.johansson@medieinstitutet.se>
2020-12-07 09:10:34 +0000

    hejdo

--- themes/cms-projekt-tema/archive-product.php
+++ themes/cms-projekt-tema/archive-product.php
@@ -1,39 +1,101 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The Template for displaying product archives, including the main shop page which is a post type archive.
  *
- * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * Override this template by copying it to yourtheme/woocommerce/archive-product.php
  *
- * @package storefront
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
  */
 
-get_header(); ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+get_header( 'shop' ); ?>
+
+	<?php
+		/**
+		 * woocommerce_before_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+
+			<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+
+		<?php endif; ?>
+
+		<?php
+			/**
+			 * woocommerce_archive_description hook
+			 *
+			 * @hooked woocommerce_taxonomy_archive_description - 10
+			 * @hooked woocommerce_product_archive_description - 10
+			 */
+			do_action( 'woocommerce_archive_description' );
+		?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<?php
+				/**
+				 * woocommerce_before_shop_loop hook
+				 *
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action( 'woocommerce_before_shop_loop' );
+			?>
+
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php woocommerce_product_subcategories(); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php wc_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
 
 			<?php
-			get_template_part( 'loop' );
+				/**
+				 * woocommerce_after_shop_loop hook
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			?>
 
-		else :
+		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
 
-			get_template_part( 'content', 'none' );
+			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
 
-		endif;
-		?>
+		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<?php
+		/**
+		 * woocommerce_after_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
 
-<?php
-do_action( 'storefront_sidebar' );
-get_footer();
+	<?php
+		/**
+		 * woocommerce_sidebar hook
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		//do_action( 'woocommerce_sidebar' );
+	?>
+
+<?php get_footer( 'shop' ); ?>
